9876172ccd25cd6de1d621b0447dd9adc13929c9
Diego Ortiz <diegortiz988@gmail.com>
2020-07-20 13:22:11 +0200

    User Model

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,13 +1,41 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    username: String,
-    password: String
-}, {
-    timestamps: true
-})
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    username: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+    occupation: {
+      type: String,
+      enum: ["Singer/Songwriter", "Producer", "DJ", "Instrumentalist"],
+    },
+    description: {
+      type: String,
+    },
+    imageUrl: {
+      type: String,
+    },
+    contactInfo: {
+      type: [String],
+    },
+    pastWork: {
+      type: [String],
+    },
+    favorites: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model("User", userSchema)
-
-module.exports = User
+const User = mongoose.model("user", UserSchema);
+module.exports = User;
